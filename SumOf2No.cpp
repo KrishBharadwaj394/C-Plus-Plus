b4d3ad336e9403d0ee3e59620bfd9148f08c7467
Krish Bharadwaj <krishbharadwaj43@gmail.com>
2025-01-27 08:03:23 +0530

    Add files via upload

---
+++ SumOf2No.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    int a,b,sum;
+    cout << "Enter first number: ";
+    cin >> a;
+    cout << "Enter second number: ";
+    cin >> b;
+    cout << "Sum of two number: " << a+b;
+}
